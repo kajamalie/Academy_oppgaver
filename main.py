159148dc015a1138c3e476e55c500cc970f2e504
kajamalie <test>
2020-08-20 10:42:34 +0200

    from_class

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 a = 4
 b = 5
 c= a
+#%%
+c = 2
+d = 1
+b = 3
 
 d = c + b 
 o = d 
+print(d)
+
+#%%
+x = 2
+print(x)
+
+#%%
+a = [1, 3, 4, 5, 7]
+
+for i in a:
+    print(i)
+
+
+for i in a:
+    print(5/i)
+
